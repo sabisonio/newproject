c9aeec072facf4d32fbea7c0a60c4df4bc00b735
Sonio <sonio@sonio.cc>
2017-01-10 16:41:22 +0800

    First commit

---
+++ app/src/main/java/com/example/sonio/newproject/login_page.java
@@ -0,0 +1,15 @@
+package com.example.sonio.newproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sonio on 2017/1/10.
+ */
+
+public class login_page extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
