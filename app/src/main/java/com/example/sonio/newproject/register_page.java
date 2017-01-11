02a6c3a14f072dcb27f2f41434e2b7c26f492c4b
Sonio <sonio@sonio.cc>
2017-01-11 11:34:54 +0800

    新增注册页！

---
+++ app/src/main/java/com/example/sonio/newproject/register_page.java
@@ -0,0 +1,16 @@
+package com.example.sonio.newproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sonio on 2017/1/11.
+ */
+
+public class register_page extends Activity{
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+
+    }
+}
